0e65ea00a219173601844f2695436d999638dda9
Asif Mahmood <asif.mahmood@iub.edu.bd>
2025-04-08 12:44:10 +0600

    Added text file input output

--- src/main/java/com/iub/oop/spring25section2/Goal2Controller.java
+++ src/main/java/com/iub/oop/spring25section2/Goal2Controller.java
@@ -5,12 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Goal2Controller
 {
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private TextArea textArea;
+    @javafx.fxml.FXML
+    private CheckBox appendCheckBox;
+
     @javafx.fxml.FXML
     public void initialize() {
     }
@@ -23,5 +31,18 @@ public class Goal2Controller
     @javafx.fxml.FXML
     public void logOut(ActionEvent actionEvent) throws IOException{
         SceneSwitcher.switchTo("login.fxml", actionEvent);
+    }
+
+    @javafx.fxml.FXML
+    public void writeToFile(ActionEvent actionEvent) throws IOException {
+        try (FileWriter writer = new FileWriter(
+                "data.txt",
+                appendCheckBox.isSelected()
+        )) {
+            writer.write(textArea.getText());
+            label.setText("File saved successfully!");
+        } catch (IOException e) {
+            label.setText("Could not save!");
+        }
     }
 }
--- src/main/java/com/iub/oop/spring25section2/Goal3Controller.java
+++ src/main/java/com/iub/oop/spring25section2/Goal3Controller.java
@@ -5,12 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Goal3Controller
 {
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private TextArea textArea;
+
     @javafx.fxml.FXML
     public void initialize() {
     }
@@ -23,5 +29,20 @@ public class Goal3Controller
     @javafx.fxml.FXML
     public void logOut(ActionEvent actionEvent) throws IOException{
         SceneSwitcher.switchTo("login.fxml", actionEvent);
+    }
+
+    @javafx.fxml.FXML
+    public void readFromFile(ActionEvent actionEvent) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))) {
+            char[] buf = new char[10];
+            while(true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                textArea.appendText(line + "\n");
+            }
+        }
+        catch (IOException e) {
+            label.setText("Could not read from file!");
+        }
     }
 }
