abcb848e7d33b0270e425f5b31c4464b005f2931
Asif Mahmood <asif.mahmood@iub.edu.bd>
2025-04-15 12:35:30 +0600

    directly read/write the whole list object

--- src/main/java/com/iub/oop/spring25section2/UserManagementController.java
+++ src/main/java/com/iub/oop/spring25section2/UserManagementController.java
@@ -129,10 +129,7 @@ public class UserManagementController {
     @FXML
     public void saveUserList(ActionEvent actionEvent) {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("data.bin"))) {
-            outputStream.writeObject(userList.size());
-            for (User u: userList) {
-                outputStream.writeObject(u);
-            }
+            outputStream.writeObject(userList);
             messageLabel.setText("Successfully saved to file.");
         } catch (IOException e) {
             e.printStackTrace();
@@ -145,14 +142,11 @@ public class UserManagementController {
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("data.bin"))) {
             userList.clear();
             tableView.getItems().clear();
-            int size = (int) inputStream.readObject();
-//            while (true) {
-            for (int i  = 0; i < size; i++) {
-                User u = (User) inputStream.readObject();
-                userList.add(u);
-                tableView.getItems().add(u);
-                System.out.println(u);
-            }
+
+            List< User > loadedList = (List<User>) inputStream.readObject();
+            userList.addAll(loadedList);
+            tableView.getItems().addAll(loadedList);
+
             messageLabel.setText("Successfully loaded data");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
